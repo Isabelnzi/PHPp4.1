4fdd38cb49dc859c297ce6ba3b85eb88b6ace198
Nzingani <isabelnzingani@outlook.com>
2018-07-27 12:28:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>P4ex1</title>
+  </head>
+  <body>
+    <?php
+    function bool(){
+      return  true;
+    }
+     echo bool();
+     ?>
+  </body>
+</html>
